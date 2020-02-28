0ea6c1b431c0cff605c76966e5421e2f6274dc7b
GeorgeRosescu1 <George1999jijel>
2020-02-28 11:25:20 +0200

    Begin Thymeleaf App with Spring, to simulate a DB registration.. currently at the begin

---
+++ src/main/java/com/georgerosescu/ThymeDB/ThymeDbApplication.java
@@ -0,0 +1,13 @@
+package com.georgerosescu.ThymeDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeDbApplication.class, args);
+	}
+
+}
