268e60fbd97b364566668073d53ed89cec849164
GeorgeRosescu1 <george.rosescu@gmail.com>
2020-03-02 16:07:19 +0200

    Added the form HTML and the controller, changed the properties file to configure the db connection

--- src/main/java/com/georgerosescu/ThymeDB/controller/GreetingController.java
+++ src/main/java/com/georgerosescu/ThymeDB/controller/GreetingController.java
@@ -2,14 +2,13 @@ package com.georgerosescu.ThymeDB.controller;
 
 import com.georgerosescu.ThymeDB.model.Customer;
 import com.georgerosescu.ThymeDB.repo.CustomerRepo;
-import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class GreetingController {
