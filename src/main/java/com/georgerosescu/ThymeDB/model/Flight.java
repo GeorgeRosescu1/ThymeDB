8fd9dcd91510efdd0afa6acdc879e873b299c214
GeorgeRosescu1 <george.rosescu@gmail.com>
2020-03-02 17:05:31 +0200

    Changed the .gitignore file

--- src/main/java/com/georgerosescu/ThymeDB/model/Flight.java
+++ src/main/java/com/georgerosescu/ThymeDB/model/Flight.java
@@ -1,4 +1,46 @@
 package com.georgerosescu.ThymeDB.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String fromCity;
+    private String toCity;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFromCity() {
+        return fromCity;
+    }
+
+    public void setFromCity(String fromCity) {
+        this.fromCity = fromCity;
+    }
+
+    public String getToCity() {
+        return toCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
